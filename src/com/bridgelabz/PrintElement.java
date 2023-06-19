57707d7b20650fd4eec18d7d415e9e3b813a54d3
Swapnil639 <swapnilpatil631997@gmail.com>
2023-06-19 21:20:32 +0530

    Added :Print element

---
+++ src/com/bridgelabz/PrintElement.java
@@ -0,0 +1,10 @@
+package com.bridgelabz;
+
+public class PrintElement {
+    public static void main(String[] args) {
+        int[] arr={10,20,70,60,35,78,98,29};
+        for (int i = 0; i <arr.length; i++) {
+            System.out.print(arr[i]+",");
+        }
+    }
+}
